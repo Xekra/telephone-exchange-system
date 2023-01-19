7eafe2b26eac2f9bbab6a0ae0d4c9352fa8cfdb0
Roman Chulyukin <r.chulyukin@gmail.com>
2023-01-20 00:00:54 +0100

    Project initialization

---
+++ src/main/java/io/xekra/telephoneexchangesystem/TelephoneExchangeSystemApplication.java
@@ -0,0 +1,13 @@
+package io.xekra.telephoneexchangesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelephoneExchangeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelephoneExchangeSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/xekra/telephoneexchangesystem/TelephoneExchangeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.xekra.telephoneexchangesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephoneExchangeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
